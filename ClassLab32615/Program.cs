f383b7288fbbbf148d77d4b7b02cfbc253af1977
justindlangley <justindlangley@gmail.com>
2015-03-26 16:44:59 -0400

    Classes
    struggling a little here

---
+++ ClassLab32615/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLab32615
+{
+    enum SubTypes
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+
+            var newStudent = new Student();
+            var myPoints = newStudent.intialPointValue + addedPoints;
+          
+        }
+    }
+    class Student
+    {
+        bool completedProgram;
+        bool helpedStudent;
+        public int intialPointValue = 0;
+        public string Subclass = "Beginner";
+        public string Subclass2 = "Grasshopper";
+        
+        
+        
+        public static int PointAllocation()
+    
+        {
+        if (completedProgram);
+        {myPoints = initialPointValue + 1};
+        if (helpedStudent)
+        else bool completedProgram = false;
+        then myPoints = initialValue + 0);
+        if helpedStudent = true;
+        myPoints = initialValue + 2
+        if helpedStudents = false;
+        myPoints = initialValue + 0
+      bool[] completedProgram = { true, false };
+      
+
+      foreach (var student in Student) 
+      {
+         int myPoints = (initialPointValue + 1);
+         Console.WriteLine("hasServiceCharge = {1}: The total is {0:C2}.", 
+                           total, hasServiceCharge); 
+
+    }
+
+
+ 
+        
+ 
+
+            
+            
+
+
+
+
+
+
+
+    }
+    
+
+}
