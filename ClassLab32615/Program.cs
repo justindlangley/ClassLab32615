9a12da2aed9edf177890319dbe4b8bf5675a8aee
justindlangley <justindlangley@gmail.com>
2015-03-27 10:07:56 -0400

    ..
    ..

--- ClassLab32615/Program.cs
+++ ClassLab32615/Program.cs
@@ -6,64 +6,95 @@ using System.Threading.Tasks;
 
 namespace ClassLab32615
 {
-    enum SubTypes
+    enum Ranking
     {
-
+        Beginner,     
+        Grasshopper,
+        Journeyman,
+        RockStar,
+        Ninja,
+        Jedi
     }
     class Program
     {
         static void Main(string[] args)
         { 
             
-
-            var newStudent = new Student();
             
-          
+            var newStudent = new netStudent();
+            int totalPoints = 0;
+            
+
+            newStudent.comletedProgram();
+            newStudent.helpedStudent();
+
+            Ranking totalPoints = Ranking.Beginner;
+            
+            if (totalPoints <= 5 && totalPoints > 0)
+            {
+                Console.WriteLine("You are a beginner");
+            }
+            
+            Ranking totalPoints = Ranking.Grasshopper;
+
+            if (totalPoints <= 10 && totalPoints > 5)
+            {
+                Console.WriteLine("You are a Grasshopper");
+            }
+
+            Ranking totalPoints = Ranking.Journeyman;
+            
+            if (totalPoints <= 15 && totalPoints > 10)
+            {
+                Console.WriteLine("You are a Journeyman");
+            }
+
+            Ranking totalPoints = Ranking.RockStar;
+
+            if (totalPoints <= 20 && totalPoints > 15)
+            {
+                Console.WriteLine("You are a Rock Star");
+            }
+
+            Ranking totalPoints = Ranking.Ninja;
+
+            if (totalPoints <=25 && totalPoints > 20)
+            {
+                Console.WriteLine("You are a Ninja");
+            }
+            Ranking totalPoints = Ranking.Jedi;
+
+            if (totalPoints > 25)
+            {
+                Console.WriteLine("You are a Jedi");
+            }
         }
-    }
-    class Student
+          
+    class netStudent
     {
-        bool completedProgram;
-        bool helpedStudent;
-        public int intialPointValue = 0;
-        public string Subclass = "Beginner";
-        public string Subclass2 = "Grasshopper";
-        int myPoints = intialPointValue + addedPoints;
         
+        public int totalPoints;
+        public string studentName { get; set; }
+              
         
-        public int PointAllocation()
-    
+        public int completedProgram() 
         {
-        if (completedProgram = true)
+        int completedProgram;
+           return totalPoints += 1;        
+        }
+
+        public bool helpedStudent();
         {
-            return int myPoints = (initialPointValue + 1)
-        };
-        if (helpedStudent = true)
-        {
-            return initialPointValue + 2};
-            
-        
-      
+          bool helpedStudent = true;
+            return totalPoints +2
 
-      
-
-    }
-
-
- 
-        
- 
-
-            
-            
-
-
-
-
+        }
 
 
 
     }
-    
 
-}
+   }
+}    
+
+
