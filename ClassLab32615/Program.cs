a49ae30b9bcb01fc37745cd709896b36f06628a8
justindlangley <justindlangley@gmail.com>
2015-03-27 13:16:29 -0400

    after bill help
    ..

--- ClassLab32615/Program.cs
+++ ClassLab32615/Program.cs
@@ -6,15 +6,8 @@ using System.Threading.Tasks;
 
 namespace ClassLab32615
 {
-    enum Ranking
-    {
-        Beginner,     
-        Grasshopper,
-        Journeyman,
-        RockStar,
-        Ninja,
-        Jedi
-    }
+    
+    
     class Program
     {
         static void Main(string[] args)
@@ -22,75 +15,92 @@ namespace ClassLab32615
             
             
             var newStudent = new netStudent();
+            
+
             int totalPoints = 0;
+
+            Console.WriteLine("How many programs have you completed today?");
+            var x = Console.ReadLine();
+            var X = int.Parse(x);
+
+            Console.WriteLine("How many students have you helped today?");
+            var y = Console.ReadLine();
+            var Y = int.Parse(y);
+            totalPoints = (X * 10) + (Y * 2);
+
+            Console.WriteLine("Your total points for today are: {0}", totalPoints);
+            Console.ReadLine();
+
+            //newStudent.calculateRank();
+            //newStudent.helpedStudent();
+
             
-
-            newStudent.comletedProgram();
-            newStudent.helpedStudent();
-
-            Ranking totalPoints = Ranking.Beginner;
             
-            if (totalPoints <= 5 && totalPoints > 0)
-            {
-                Console.WriteLine("You are a beginner");
-            }
-            
-            Ranking totalPoints = Ranking.Grasshopper;
-
-            if (totalPoints <= 10 && totalPoints > 5)
-            {
-                Console.WriteLine("You are a Grasshopper");
-            }
-
-            Ranking totalPoints = Ranking.Journeyman;
-            
-            if (totalPoints <= 15 && totalPoints > 10)
-            {
-                Console.WriteLine("You are a Journeyman");
-            }
-
-            Ranking totalPoints = Ranking.RockStar;
-
-            if (totalPoints <= 20 && totalPoints > 15)
-            {
-                Console.WriteLine("You are a Rock Star");
-            }
-
-            Ranking totalPoints = Ranking.Ninja;
-
-            if (totalPoints <=25 && totalPoints > 20)
-            {
-                Console.WriteLine("You are a Ninja");
-            }
-            Ranking totalPoints = Ranking.Jedi;
-
-            if (totalPoints > 25)
-            {
-                Console.WriteLine("You are a Jedi");
-            }
-        }
+        }   
           
     class netStudent
     {
         
         public int totalPoints;
         public string studentName { get; set; }
-              
-        
-        public int completedProgram() 
+
+
+        public enum Ranking
         {
-        int completedProgram;
-           return totalPoints += 1;        
+            Beginner,
+            Grasshopper,
+            Journeyman,
+            RockStar,
+            Ninja,
+            Jedi
         }
 
-        public bool helpedStudent();
+        private Ranking level;
+
+        public void calculateRank()
         {
-          bool helpedStudent = true;
-            return totalPoints +2
+            if (totalPoints <= 5 && totalPoints > 0)
+            {
+                Console.WriteLine("You are a beginner");
+                this.level = Ranking.Beginner;
+            }
+
+
+            if (totalPoints <= 10 && totalPoints > 5)
+            {
+                Console.WriteLine("You are a Grasshopper");
+                this.level = Ranking.Grasshopper;
+     
+            }
+
+
+
+            if (totalPoints <= 15 && totalPoints > 10)
+            {
+                Console.WriteLine("You are a Journeyman");
+                this.level = Ranking.Journeyman;
+            }
+
+            if (totalPoints <= 20 && totalPoints > 15)
+            {
+                Console.WriteLine("You are a Rock Star");
+                this.level = Ranking.RockStar;
+            }
+
+
+            if (totalPoints <= 25 && totalPoints > 20)
+            {
+                Console.WriteLine("You are a Ninja");
+                this.level = Ranking.Ninja;
+            }
+
+            if (totalPoints > 25)
+            {
+                Console.WriteLine("You are a Jedi");
+                this.level = Ranking.Jedi;
+            }
 
         }
-
-
 
     }
 
